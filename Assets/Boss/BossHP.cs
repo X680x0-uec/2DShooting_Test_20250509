1bc2b2b11e39574715cc1d0ca0f18fd12e9842d7
25-yoshida <qingzai990@gmail.com>
2025-10-13 17:23:53 +0900

    ボスのHPを設定

---
+++ Assets/Boss/BossHP.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BossHP : MonoBehaviour
+{
+    public SkillSystem skillSystem;
+    public float maxHP = 100;
+    private float currentHP;
+    public int pointValue = 100;
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // ダメージを受ける関数
+    public void TakeDamage(float damage)
+    {
+        currentHP -= damage;
+        Debug.Log("敵の現在HP: " + currentHP);
+
+        if (currentHP <= 0)
+        {
+            BossDie();
+        }
+    }
+
+    // 敵が死ぬときの処理
+    void BossDie()
+    {
+        skillSystem.TakeSkillPoint(pointValue);
+        Destroy(gameObject);
+    }
+}
