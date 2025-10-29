ef7421c23645e3bd190a7d8cfed664b67efd5ecf
25SN <n2512162@gl.cc.uec.ac.jp>
2025-10-29 23:15:41 +0900

    プルするため

--- Assets/Player/Laser.cs
+++ Assets/Player/Laser.cs
@@ -1,13 +1,84 @@
 using UnityEngine;
+using System.Collections;
 
 public class Laser : MonoBehaviour
 {
     public float damage = 1f;
     public float damageInterval = 0.1f;
+    public float appearDuration = 0.1f;
+    public float disappearDuration = 0.15f;
+
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+    private Vector3 originalScale;
 
     // このレーザーに触れている敵を管理するリスト
     private System.Collections.Generic.List<ZakoHP> enemiesInRange = new System.Collections.Generic.List<ZakoHP>();
     private float nextDamageTime;
+
+    private IEnumerator Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        originalScale = transform.localScale;
+
+        boxCollider.enabled = false;
+
+        yield return StartCoroutine(AppearAnimation());
+
+        boxCollider.enabled = true;
+    }
+
+    private IEnumerator AppearAnimation()
+    {
+        float timer = 0f;
+        while (timer < appearDuration)
+        {
+            float progress = timer / appearDuration;
+
+            float currentYScale = Mathf.Lerp(0, originalScale.y, progress);
+            transform.localScale = new Vector3(originalScale.x, currentYScale, originalScale.z);
+
+            Color color = spriteRenderer.color;
+            color.a = Mathf.Lerp(0, 0.8f, progress);
+            spriteRenderer.color = color;
+
+            timer += Time.deltaTime;
+            yield return null; //1フレーム待機
+        }
+        transform.localScale = originalScale;
+        Color finalColor = spriteRenderer.color;
+        finalColor.a = 0.8f;
+        spriteRenderer.color = finalColor;
+    }
+
+    public void StartDisappearAnimation()
+    {
+        StopAllCoroutines();
+        StartCoroutine(DisappearAnimation());
+    }
+
+    private IEnumerator DisappearAnimation()
+    {
+        boxCollider.enabled = false;
+        this.enabled = false;
+
+        float timer = 0f;
+        while (timer < disappearDuration)
+        {
+            float progress = timer / disappearDuration;
+            float currentYScale = Mathf.Lerp(originalScale.y, 0, progress);
+            transform.localScale = new Vector3(originalScale.x, currentYScale, originalScale.z);
+            Color color = spriteRenderer.color;
+            color.a = Mathf.Lerp(0.8f, 0, progress);
+            spriteRenderer.color = color;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
+    }
 
     // Update is called once per frame
     void Update()
